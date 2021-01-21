1dbdd05a7bf1383b6a6df6e9c742131f425772dc
Alan <alan.gaia@mercadolibre.com>
2021-01-21 13:31:50 -0300

    first commit with errors

---
+++ introJava/src/ejercicios/NumerosPares.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+
+
+public class NumerosPares {
+
+    public static boolean esPar(int n){
+        return n%2==0;
+    }
+
+    public static void Main(String [] args){
+
+        System.out.println(esPar(3));
+    }
+}
+
+
+
