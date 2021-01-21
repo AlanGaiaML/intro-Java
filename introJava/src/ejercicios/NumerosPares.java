9ae85af6bb542ae1a81d60b2aacf357b77e83229
Alan <alan.gaia@mercadolibre.com>
2021-01-21 14:09:34 -0300

    primer ejercicio resuelto

--- introJava/src/ejercicios/NumerosPares.java
+++ introJava/src/ejercicios/NumerosPares.java
@@ -1,16 +1,25 @@
 package ejercicios;
 
 
+import java.util.Scanner;
 
 public class NumerosPares {
 
-    public static boolean esPar(int n){
-        return n%2==0;
-    }
 
-    public static void Main(String [] args){
+    public static void main(String [] args){
+        //Generando un Scanner para leer Input
+        Scanner scanner = new Scanner(System.in);
+        //mensaje previo a capturar el input
+        System.out.println("Ingrese número entero");
+        //capturo el input
+        int numero = scanner.nextInt();
+        //scaner close?
+        scanner.close();
+        //paso el como parametro el valor
+        for (int i = 1; i <= numero; i++) {
+            System.out.println(i*2);
+        }
 
-        System.out.println(esPar(3));
     }
 }
 
